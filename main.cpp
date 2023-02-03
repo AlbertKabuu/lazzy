a993a88ad5b5967b6f7efecb3d2e52de7898bfd9
lazyrrre <lazyrrrrererere@gmail.com>
2023-02-03 07:41:48 +0300

    test

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@ using namespace std;
 
 class TPen{
 	private:
-	
+		int _color;
+		
 	public:
-	
+		void setColor(int _colorcode);
+		string getColor();
 };
 
+void setColor(int _colorcode){
+	_color = code
+}
 
 int main(){
 	cout << "Hello";
